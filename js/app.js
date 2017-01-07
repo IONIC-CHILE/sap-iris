c95348c22c445a750c04b2cdd97d8d472eb9f329
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-01-07 18:28:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 
 
 
-  $http.get('http://170.239.84.214/dropbox/sap/sap-iris-server/index-usuarios.php')
+  $http.get('https://test-chile.github.io/test/datos/usuarios.json')
        .success(function(result){
            $scope.usuarios = result;
 
